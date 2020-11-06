e32aa2a2ae757ad335269d85c0a24c21a290c8bf
Pranali Deshmukh <deshmuk3@coventry.ac.uk>
2020-11-06 07:22:51 +0530

    feat(db): adds router methods for Artists
    Adds get, post, put and delete methods for the Artists Schema.
    
    ref #3

--- api/artists/artist.routes.js
+++ api/artists/artist.routes.js
@@ -8,9 +8,11 @@ module.exports = Router => {
   });
 
   router
-    .get('/:artistId', controller.getOne)
+    .get('/:id', controller.getOne)
     .get('/', controller.getAll)
-    .post('/', controller.createOne);
+    .post('/', controller.createOne)
+    .put('/:id', controller.modifyOne)
+    .delete('/:id', controller.deleteOne);
 
   return router;
 };
